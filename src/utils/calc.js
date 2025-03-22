991982eb12f60c1c0bfbe64986f00872a75e4772
RyabicovNikita <RyabicovN1k@yandex.ru>
2025-03-22 19:17:25 +0300

    fix calc prePrice

--- src/utils/calc.js
+++ src/utils/calc.js
@@ -2,6 +2,8 @@ export const getTripPrePrice = (begDriverPrice = 0, begPassengerPrice = 0) => {
   const driverPrice = Number(begDriverPrice);
   const passengerPrice = Number(begPassengerPrice);
 
+  if (driverPrice === passengerPrice) return passengerPrice;
+
   if (driverPrice === 0 || isNaN(driverPrice)) {
     if (passengerPrice === 0 || isNaN(passengerPrice)) return 0;
     return passengerPrice;
