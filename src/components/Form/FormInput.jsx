7cd319422b6a8887cc5af1ed5851a5cb423e7b9a
RyabicovNikita <RyabicovN1k@yandex.ru>
2025-03-22 11:41:02 +0300

    add new error form

--- src/components/Form/FormInput.jsx
+++ src/components/Form/FormInput.jsx
@@ -1,15 +1,18 @@
 import { Form } from "react-bootstrap";
 
-export const FormInput = ({ key, className, error, children, type = "string", props, placeholder = "" }) => (
-  <div className={className}>
-    {children && <Form.Label htmlFor={key}>{children}</Form.Label>}
-    <Form.Control
-      className={`${error ? "is-invalid" : ""}`}
-      id={key}
-      name={key}
-      type={type}
-      placeholder={placeholder}
-      {...props}
-    />
-  </div>
-);
+export const FormInput = ({ id, className, error, children, type = "string", props, placeholder = "" }) => {
+  const curError = error?.fields ? error?.fields?.includes(id) : error;
+  return (
+    <div className={className}>
+      {children && <Form.Label htmlFor={id}>{children}</Form.Label>}
+      <Form.Control
+        className={`${curError ? "is-invalid" : ""}`}
+        id={id}
+        name={id}
+        type={type}
+        placeholder={placeholder}
+        {...props}
+      />
+    </div>
+  );
+};
--- src/components/Form/FormSelector.jsx
+++ src/components/Form/FormSelector.jsx
@@ -1,13 +1,16 @@
 import { FloatingLabel, FormSelect, InputGroup } from "react-bootstrap";
 
-export const FormSelector = ({ children, props, options = [], className, error }) => (
-  <InputGroup className={`mb-3 ${className}`}>
-    <FloatingLabel label={children}>
-      <FormSelect {...props} className={`${error ? "is-invalid" : ""} `}>
-        {options.map((option) => (
-          <option>{option}</option>
-        ))}
-      </FormSelect>
-    </FloatingLabel>
-  </InputGroup>
-);
+export const FormSelector = ({ id, children, props, options = [], className, error }) => {
+  const curError = error?.fields ? error?.fields?.includes(id) : error;
+  return (
+    <InputGroup className={`mb-3 ${className}`}>
+      <FloatingLabel label={children}>
+        <FormSelect {...props} className={`${curError ? "is-invalid" : ""} `}>
+          {options.map((option) => (
+            <option>{option}</option>
+          ))}
+        </FormSelect>
+      </FloatingLabel>
+    </InputGroup>
+  );
+};
