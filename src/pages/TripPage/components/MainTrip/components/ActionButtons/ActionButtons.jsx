a565aa3caf7f46f48d7ecbe61281c48d1ccfbf15
RyabicovNikita <RyabicovN1k@yandex.ru>
2025-04-02 20:27:50 +0300

    add media query

--- src/pages/TripPage/components/MainTrip/components/ActionButtons/ActionButtons.jsx
+++ src/pages/TripPage/components/MainTrip/components/ActionButtons/ActionButtons.jsx
@@ -1,3 +1,4 @@
+import "./ActionButtons.scss"
 import { Button } from "react-bootstrap";
 import { ROLES, TRIP_STATUSES } from "../../../../../../constants";
 import { Link } from "react-router";
@@ -5,13 +6,13 @@ import { Link } from "react-router";
 export const ActionButtons = ({ trip, isDriver, userID, role, actions }) => {
   const { onApproveDriverClick, onUntieDriverClick, onCancelTripClick, onEditTripClick, onDeleteTripClick } = actions;
   return (
-    <div className="d-flex gap-5">
+    <div className="actions d-flex">
       {!isDriver && trip?.status === TRIP_STATUSES.CORRECTED_PRICE.text && trip?.creator?.id === userID && (
         <>
-          <Button onClick={onApproveDriverClick} className="btn-success">
+          <Button onClick={onApproveDriverClick} className="actions__action btn-success">
             Утвердить водителя
           </Button>
-          <Button onClick={onUntieDriverClick} className="btn-warning">
+          <Button onClick={onUntieDriverClick} className="actions__action btn-warning">
             Отвязать водителя
           </Button>
         </>
@@ -19,17 +20,17 @@ export const ActionButtons = ({ trip, isDriver, userID, role, actions }) => {
       {trip?.creator?.id === userID &&
         trip?.status !== TRIP_STATUSES.CANCEL.text &&
         trip?.status !== TRIP_STATUSES.SUCCESSFUL.text && (
-          <Button onClick={onCancelTripClick} className="btn-danger">
+          <Button onClick={onCancelTripClick} className="actions__action btn-danger">
             Отменить поездку
           </Button>
         )}
       {trip?.creator?.id === userID && trip?.status === TRIP_STATUSES.NEW.text && (
-        <Link className="btn bg-dark text-light border-dark p-3" onClick={onEditTripClick}>
+        <Link className="actions__action btn bg-dark text-light border-dark p-3" onClick={onEditTripClick}>
           Редактировать поездку
         </Link>
       )}
       {role === ROLES.ADMIN && trip?.id && (
-        <button className="btn bg-dark text-light border-dark p-3" onClick={onDeleteTripClick}>
+        <button className="actions__action btn bg-dark text-light border-dark p-3" onClick={onDeleteTripClick}>
           Удалить поездку
         </button>
       )}
