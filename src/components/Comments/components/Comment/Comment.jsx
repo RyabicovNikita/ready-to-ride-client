b100ef2aaafa30d1faa6fecb2f97b6bc23d746ff
RyabicovNikita <RyabicovN1k@yandex.ru>
2025-04-25 21:47:47 +0300

    personal icon user in comments

--- src/components/Comments/components/Comment/Comment.jsx
+++ src/components/Comments/components/Comment/Comment.jsx
@@ -1,10 +1,12 @@
 import "./Comment.scss";
 import { mergeStringsBySpace } from "../../../../formatters/formatString";
+import defaultUserIcon from "../../../../images/ghost.png";
 export const Comment = ({ created_at, user, children: text }) => {
+
   return (
     <div className="comment mb-3">
       <div className="comment__content" style={{ justifyContent: user.isDriver ? "flex-end" : "flex-start" }}>
-        <img class="comment__icon" alt="Bootstrap Media Preview" src="https://i.imgur.com/stD0Q19.jpg" />
+        <img class="comment__icon" alt="Bootstrap Media Preview" src={user.imageUrl || defaultUserIcon} />
         <div className="comment__message">
           <div className="comment__message-whenSend">
             <span>{mergeStringsBySpace([created_at.date, created_at.time])}</span>
